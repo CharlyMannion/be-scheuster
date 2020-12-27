ece1e5504f010629e965955512bd2b3c5f94bc43
CharlyMannion <charlottemannion100@gmail.com>
2020-12-27 17:39:01 +0000

    attempts to fix connection

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,7 @@ const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 
 const dbConfig =
-    ENV === 'production' ?
-    { client: 'pg', connection: process.env.DATABASE_URL } :
+    ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } :
     require('../knexfile');
 
 module.exports = knex(dbConfig);
