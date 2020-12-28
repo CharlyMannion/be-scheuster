77ab8ce50e73eddc08c1c82f6fd4588c99680109
CharlyMannion <charlottemannion100@gmail.com>
2020-12-28 13:10:52 +0000

    adds possible routes to /api

--- routes/api.router.js
+++ routes/api.router.js
@@ -5,4 +5,15 @@ const userRouter = require('./users.router');
 apiRouter.use('/shoes', shoesRouter);
 apiRouter.use('/users', userRouter);
 
+apiRouter.get('/', (req, res) => {
+    res.json({
+        '/api/shoes': ['GET', 'POST'],
+        '/api/shoes/:shoe_id': ['GET', 'PATCH', 'DELETE'],
+        '/api/users': ['GET', 'POST'],
+        '/api/users/:user_id': ['GET', 'PATCH', 'DELETE'],
+        '/api/orders/': ['POST', 'GET'],
+        '/api/orders/:order_id': ['PATCH', 'GET'],
+    })
+})
+
 module.exports = apiRouter;
